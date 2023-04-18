6c6984bf8b1daa39b8d095fb612f4d603f922b40
Saqib Ansari <saqib.ns111@gmail.com>
2023-04-18 11:03:48 +0500

    setting of app

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is funcare app business version</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/images.js
@@ -0,0 +1,25 @@
+export const beach = require("../assets/images/beach.jpeg");
+export const climbingHills = require("../assets/images/climbing_hills.jpg");
+export const frozenHills = require("../assets/images/frozen_hills.jpg");
+export const homeBanner = require("../assets/images/home_banner.jpg");
+export const onboardingImage = require("../assets/images/onboarding_image.jpg");
+export const skiVilla = require("../assets/images/ski_villa.jpg");
+export const skiVillaBanner = require("../assets/images/ski_villa_banner.jpg");
+export const onboardingImage1 = require("../assets/images/onboarding_image1.webp");
+export const joyland = require("../assets/images/joyland.jpg");
+export const phone = require("../assets/images/phone.png");
+export const verification = require("../assets/images/verification.png");
+
+export default {
+  beach,
+  climbingHills,
+  frozenHills,
+  homeBanner,
+  onboardingImage,
+  skiVilla,
+  skiVillaBanner,
+  onboardingImage1,
+  joyland,
+  phone,
+  verification,
+};
